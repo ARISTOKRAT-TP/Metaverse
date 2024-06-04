1147424ebac38b2bdbeae7fb40374ead3f7f789a
Artyom <61425754+ARISTOKRAT-TP@users.noreply.github.com>
2024-06-04 22:08:21 +0300

    fix scale

--- VRShared/Assets/HydroApp/Scripts/ScaleChange.cs
+++ VRShared/Assets/HydroApp/Scripts/ScaleChange.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,40 +12,19 @@ public class ScaleChange : NetworkBehaviour
 {
     [SerializeField]
     [Tooltip("Values to change scale")]
-    public GameObject pipe1, pipe2, pipe3, pipe4, pipe5, popl_up, popl_down;
+    public GameObject[] pipes;
+    public GameObject popl_up, popl_down;
     public XRKnob xRKnob;
     public XRKnob dial;
     public XRKnob latr;
-    public TextMeshProUGUI textMesh;
-    public TextMeshProUGUI textMesh2;
-    public TextMeshProUGUI textMesh3;
-    public TextMeshProUGUI textMesh4;
-    public TextMeshProUGUI textMesh5;
+    public TextMeshProUGUI[] textsMesh;
     public TextMeshProUGUI temperature;
     public AudioSource audioSource;
     private int temperaturevalue;
-
-    public Toggle checkboxStep2;
-    public Toggle checkboxStep3;
-    public Toggle checkboxStep4;
-    public Toggle checkboxStep5;
-    public Toggle checkboxStep6;
-    public Toggle checkboxStep7;
-    public Toggle checkboxStep8;
-
-    [Networked]
-    public float size1 { get; set; }
-    [Networked]
-    public float size2 { get; set; }
-    [Networked]
-    public float size3 { get; set; }
-    [Networked]
-    public float size4 { get; set; }
-    [Networked]
-    public float size5 { get; set; }
-
+    public float[] sizes = new float[5];
     public XRLever toggle;
     public float durationWater;
+    public Toggle[] checkboxes;
 
     void Start()
     {
@@ -54,52 +32,19 @@ public class ScaleChange : NetworkBehaviour
         temperature.text = "Температура воды:" + temperaturevalue.ToString() + " C";
     }
 
+    public void fakeOnClick()
+    {
+        if (checkboxes[2].isOn == true && checkboxes[1].isOn == true)
+        {
+            checkboxes[3].isOn = true;
+        }
+    }
+
     public void ChangeScalePipes()
     {
         if (toggle.value == true && dial.value < 0.15)
         {
             Rpc_ChangeScale(dial.value, latr.value, xRKnob.value, toggle.value);
-            checkboxStep2.isOn = true;
-
-            if(latr.value > 0)
-            {
-                checkboxStep3.isOn = true;
-            } else if(checkboxStep6.isOn == false)
-            {
-                checkboxStep3.isOn = false;
-                checkboxStep4.isOn = false;
-                checkboxStep5.isOn = false;
-                checkboxStep6.isOn = false;
-            }
-            if(1 - xRKnob.value > 0 && checkboxStep3.isOn == true)
-            {
-                checkboxStep4.isOn = true;
-            } else if(1 - xRKnob.value <= 0 && checkboxStep5.isOn == true && checkboxStep4.isOn == true)
-            {
-                checkboxStep6.isOn = true;
-            } else
-            {
-                checkboxStep4.isOn = false;
-                checkboxStep5.isOn = false;
-                checkboxStep6.isOn = false;
-            }
-            if(checkboxStep5.isOn == true && checkboxStep6.isOn == true && latr.value == 0) {
-                checkboxStep7.isOn = true;
-            }
-
-        }   else if (dial.value < 0.65 && dial.value > 0.45)
-        {
-            if(checkboxStep7.isOn == true){
-                checkboxStep8.isOn = true;
-            } else {
-                checkboxStep2.isOn = false;
-                checkboxStep3.isOn = false;
-                checkboxStep4.isOn = false;
-                checkboxStep5.isOn = false;
-                checkboxStep6.isOn = false;
-                checkboxStep7.isOn = false;
-                checkboxStep8.isOn = false;
-            }
         }
         else
         {
@@ -110,58 +55,134 @@ public class ScaleChange : NetworkBehaviour
     [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
     private void Rpc_ChangeScale(float dialValue, float latrValue, float xRKnobValue, bool toggleValue)
     {
+        Vector3[] newSize = new Vector3[pipes.Length];
+        
+        // Обновление состояния чекбоксов
+        UpdateCheckboxes(dialValue, latrValue, xRKnobValue, toggleValue);
+
         if (dialValue < 0.15 && toggleValue == true)
         {
-            size1 = (101f / 1960f) * 1.77f * 1.77f * latrValue * 300 * (1 - xRKnobValue) + 19.5f;
-            size2 = (81f / 1960f) * 1.43f * 1.43f * latrValue * 300 * (1 - xRKnobValue) + 19.5f;
-            size3 = (-35f / 1960f) * 1.13f * 1.13f * latrValue * 300 * (1 - xRKnobValue) + 19.5f;
-            size4 = (2.5f / 1960f) * 1.54f * 1.54f * latrValue * 300 * (1 - xRKnobValue) + 19.5f;
-            size5 = (13f / 1960f) * 2.69f * 2.69f * latrValue * 300 * (1 - xRKnobValue) + 19.5f;
-            UpdateSizes();
+            sizes[0] = (101f / 1960f) * 1.77f * 1.77f * latrValue * 300 * (1 - xRKnobValue) + 19.5f;
+            sizes[1] = (81f / 1960f) * 1.43f * 1.43f * latrValue * 300 * (1 - xRKnobValue) + 19.5f;
+            sizes[2] = (-35f / 1960f) * 1.13f * 1.13f * latrValue * 300 * (1 - xRKnobValue) + 19.5f;
+            sizes[3] = (2.5f / 1960f) * 1.54f * 1.54f * latrValue * 300 * (1 - xRKnobValue) + 19.5f;
+            sizes[4] = (13f / 1960f) * 2.69f * 2.69f * latrValue * 300 * (1 - xRKnobValue) + 19.5f;
+
+            if (latrValue * 300 * 0.8 * (1 - xRKnobValue) < 125 && latrValue * 300 * 0.8 * (1 - xRKnobValue) > 0)
+            {
+                popl_up.transform.position = new Vector3(-14.3615999f, 1.4429f + ((latrValue * 2.3f * 0.8f * (1 - xRKnobValue)) * 0.3099f), 8.84440041f);
+            }
+            else if ((latrValue * 300 * 0.8 * (1 - xRKnobValue)) >= 125)
+            {
+                popl_down.transform.position = new Vector3(-14.3615999f, 1.4429f + (((latrValue) * 2.3f * 0.8f * (1 - xRKnobValue)) * 0.3229f) - 0.3f, 8.84440041f);
+            }
+            audioSource.volume = latrValue * 0.35f;
+
+            for (int i = 0; i < pipes.Length; i++)
+            {
+                newSize[i] = new Vector3(pipes[i].transform.localScale.x, sizes[i] / 19.5f, pipes[i].transform.localScale.z);
+                pipes[i].transform.localScale = newSize[i];
+                textsMesh[i].text = Math.Round(sizes[i], 2).ToString() + "cm";
+            }
         }
         else
         {
             Rpc_ResetScale();
         }
+
+        Rpc_UpdateTextMeshes(sizes);
     }
 
     [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
     private void Rpc_ResetScale()
     {
-        size1 = 19.5f;
-        size2 = 19.5f;
-        size3 = 19.5f;
-        size4 = 19.5f;
-        size5 = 19.5f;
-        UpdateSizes();
+        Vector3[] newSize = new Vector3[pipes.Length];
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            sizes[i] = 19.5f;
+            newSize[i] = new Vector3(pipes[i].transform.localScale.x, sizes[i] / 19.5f, pipes[i].transform.localScale.z);
+            pipes[i].transform.localScale = newSize[i];
+            textsMesh[i].text = Math.Round(sizes[i], 2).ToString() + "cm";
+        }
+
+        Rpc_UpdateTextMeshes(sizes);
     }
 
-    private void UpdateSizes()
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void Rpc_UpdateTextMeshes(float[] updatedSizes)
     {
-        Vector3 newSize1 = new Vector3(pipe1.transform.localScale.x, size1 / 19.5f, pipe1.transform.localScale.z);
-        Vector3 newSize2 = new Vector3(pipe2.transform.localScale.x, size2 / 19.5f, pipe2.transform.localScale.z);
-        Vector3 newSize3 = new Vector3(pipe3.transform.localScale.x, size3 / 19.5f, pipe3.transform.localScale.z);
-        Vector3 newSize4 = new Vector3(pipe4.transform.localScale.x, size4 / 19.5f, pipe4.transform.localScale.z);
-        Vector3 newSize5 = new Vector3(pipe5.transform.localScale.x, size5 / 19.5f, pipe5.transform.localScale.z);
+        for (int i = 0; i < updatedSizes.Length; i++)
+        {
+            textsMesh[i].text = Math.Round(updatedSizes[i], 2).ToString() + "cm";
+        }
+    }
 
-        pipe1.transform.localScale = newSize1;
-        pipe2.transform.localScale = newSize2;
-        pipe3.transform.localScale = newSize3;
-        pipe4.transform.localScale = newSize4;
-        pipe5.transform.localScale = newSize5;
+    private void UpdateCheckboxes(float dialValue, float latrValue, float xRKnobValue, bool toggleValue)
+    {
+        if (toggleValue == true && dialValue < 0.15)
+        {
+            checkboxes[0].isOn = true;
 
-        textMesh.text = Math.Round(size1, 2).ToString() + "cm";
-        textMesh2.text = Math.Round(size2, 2).ToString() + "cm";
-        textMesh3.text = Math.Round(size3, 2).ToString() + "cm";
-        textMesh4.text = Math.Round(size4, 2).ToString() + "cm";
-        textMesh5.text = Math.Round(size5, 2).ToString() + "cm";
+            if (latrValue > 0)
+            {
+                checkboxes[1].isOn = true;
+            }
+            else if (checkboxes[4].isOn == false)
+            {
+                checkboxes[1].isOn = false;
+                checkboxes[2].isOn = false;
+                checkboxes[3].isOn = false;
+                checkboxes[4].isOn = false;
+            }
+
+            if (1 - xRKnobValue > 0 && checkboxes[1].isOn == true)
+            {
+                checkboxes[2].isOn = true;
+            }
+            else if (1 - xRKnobValue <= 0 && checkboxes[3].isOn == true && checkboxes[2].isOn == true)
+            {
+                checkboxes[4].isOn = true;
+            }
+            else
+            {
+                checkboxes[2].isOn = false;
+                checkboxes[3].isOn = false;
+                checkboxes[4].isOn = false;
+            }
+
+            if (checkboxes[3].isOn == true && checkboxes[4].isOn == true && latrValue == 0)
+            {
+                checkboxes[5].isOn = true;
+            }
+        }
+        else if (dialValue < 0.65 && dialValue > 0.45)
+        {
+            if (checkboxes[5].isOn == true)
+            {
+                checkboxes[6].isOn = true;
+            }
+            else
+            {
+                for (int i = 0; i < checkboxes.Length; i++)
+                {
+                    checkboxes[i].isOn = false;
+                }
+            }
+        }
     }
 
     public override void FixedUpdateNetwork()
     {
         if (Object.HasStateAuthority)
         {
-            UpdateSizes();
+            // Обновление размеров труб на владельце состояния
+            Vector3[] newSize = new Vector3[pipes.Length];
+            for (int i = 0; i < pipes.Length; i++)
+            {
+                newSize[i] = new Vector3(pipes[i].transform.localScale.x, sizes[i] / 19.5f, pipes[i].transform.localScale.z);
+                pipes[i].transform.localScale = newSize[i];
+                textsMesh[i].text = Math.Round(sizes[i], 2).ToString() + "cm";
+            }
         }
     }
 }
